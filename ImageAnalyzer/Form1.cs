3c584fdccee93fff31ecbd653d8ed19822890655
KMSwarner <46699418+KMSwarner@users.noreply.github.com>
2019-04-10 23:45:03 -0400

    Initial Functionality
    Prototype version functionality prior to threading experiments.

--- ImageAnalyzer/Form1.cs
+++ ImageAnalyzer/Form1.cs
@@ -8,6 +8,7 @@ using System.IO;
 using System.Linq;
 using System.Security;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -47,14 +48,24 @@ namespace ImageAnalyzer
     public partial class Form1 : Form
     {
         private OpenFileDialog loadFile;
-        Image newImage;
+        Image newImage = null;
+        Image imgDefaultImage;
         Size szImageSize;
         //Size szScaledSize;
         int imageDimensions = 0;
+        int checksum = 0;
+
+        //Thread thdAnalyzeImageF = new Thread(AnalyzeImageForward);
+        //Thread thdAnalyzeImageL = new Thread(AnalyzeImageBackward);
+        //Thread thdAnalyzeImageR = new Thread(AnalyzeImageMiddleOut());
+        //Thread thdAnalyzeImageB = new Thread(AnalyzeImageMiddleOut(1));
+
+        //Thread t = new Thread (new ParameterizedThreadStart(myMethod));
+        //t.Start(imageDimensions);
 
         TreeNode colorTree = new TreeNode();
 
-        public StructColors[,] pixelColorRA = new StructColors[0,0];
+        public StructColors[,] pixelColorRA = new StructColors[0, 0];
 
         public struct StructColors
         {
@@ -65,9 +76,38 @@ namespace ImageAnalyzer
             public string strHexValue;
         }
 
+
+
+        //public Thread StartTheThread(int param1, ref int param2)
+        //{
+        //    var t = new Thread(() => RealStart(param1, param2));
+        //    t.Start();
+        //    return t;
+        //}
+
+        //private static void RealStart(SomeType param1, ref int param2)
+        //{
+        //}
+  
+        //private static void AnalyzeImageForward ()
+        //{
+
+        //}
+        //private static void AnalyzeImageBackward()
+        //{
+
+        //}
+
+        //private static void AnalyzeImageMiddleOut(int direction)
+        //{
+
+        //}
+
         public Form1()
         {
             InitializeComponent();
+
+            imgDefaultImage = picbxImage.Image;
 
             loadFile = new OpenFileDialog();
             {
@@ -86,18 +126,125 @@ namespace ImageAnalyzer
             int mostIndexedColor = 0;
             string hexString = "";
 
-            loadImage();
-            IndexPixelColors(ref pixelColorRA);
-            BuildColorTree(ref colorTree, pixelColorRA);
-            CompareColorIndex(colorTree, ref mostIndexedColor, ref hexString);
+            StructColors col = new StructColors();
+            //Thread thdIndexForward = new Thread(IndexPixelColors);
+            //Thread thdIndexBackward = new Thread(IndexPixelColors);
 
-            txtMostIndexedColor.Text = "#" + hexString + "x";
-            txtTimesIndexed.Text = mostIndexedColor.ToString();
+            loadImage();
+
+            if (newImage != null)
+            {
+                IndexPixelColors(ref pixelColorRA);
+
+                //pixelColorRA = new StructColors[newImage.Height, newImage.Width];
+
+                //thdIndexForward.Start(1);
+                //thdIndexBackward.Start(-1);
+
+                //while (checksum != 2)
+                //{
+
+                //}
+
+                BuildColorTree(ref colorTree, pixelColorRA);
+                CompareColorIndex(colorTree, ref mostIndexedColor, ref hexString);
+
+                col = PopulateStructFromHex(hexString);
+
+                txtMostIndexedColor.Text = "#" + hexString;
+                txtTimesIndexed.Text = mostIndexedColor.ToString();
+                createSwatch(col);
+            }
+
+            else
+            {
+                lblStatusLabel.Text = "Warning: No image selected!";
+            }
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
+        private void createSwatch(StructColors colorInfo)
         {
+            Bitmap bmp = new Bitmap(96, 96);
+            Graphics gfx = Graphics.FromImage(bmp);
+            SolidBrush brush;
+
+            brush = new SolidBrush(Color.FromArgb(colorInfo.R, colorInfo.G, colorInfo.B));
+
+            gfx.FillRectangle(brush, 0, 0, 96, 96);
+
+            picbxSwatch.Image = bmp;
+        }
+
+        private StructColors PopulateStructFromHex(string strHexValue)
+        {
+            StructColors col = new StructColors();
+
+            col.R = 16 * ParseHexValue(strHexValue[0]) + ParseHexValue(strHexValue[1]);
+            col.G = 16 * ParseHexValue(strHexValue[2]) + ParseHexValue(strHexValue[3]);
+            col.B = 16 * ParseHexValue(strHexValue[4]) + ParseHexValue(strHexValue[5]);
+
+            col.strHexValue = strHexValue;
+
+            return col;
+        }
+
+        private int ParseHexValue(char chrHex)
+        {
+            if (chrHex >= '0' && chrHex <= '9')
+            {
+                return chrHex - 48;
+            }
+
+            else if (chrHex >= 65 && chrHex <= 70)
+            {
+                return chrHex - 55;
+            }
+
+            return 0;
+        }
+
+        private void picbxImage_Click(object sender, EventArgs e)
+        {
+            FormReset();
+
             DoTheThings();
+        }
+
+        private void picbxSwatch_Click(object sender, EventArgs e)
+        {
+            if (txtMostIndexedColor.Text != "")
+            {
+                Clipboard.SetText(txtMostIndexedColor.Text);
+
+                lblStatusLabel.Text = "Hex value copied to clipboard.";
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            FormReset();
+        }
+
+        private void FormReset()
+        {
+            //CLEAR ALL FIELDS
+            txtHeight.Text = "";
+            txtWidth.Text = "";
+            txtTimesIndexed.Text = "";
+            txtMostIndexedColor.Text = "";
+
+            lblMessageLabel.Text = "";
+            lblStatusLabel.Text = "Form cleared.";
+
+            if(picbxImage.Image != null)
+            {
+                picbxImage.Image.Dispose();
+            }
+            
+            picbxImage.Image = imgDefaultImage;
+            picbxSwatch.Image = imgDefaultImage;
+
+            newImage = null;
         }
 
         private void loadImage()
@@ -109,12 +256,14 @@ namespace ImageAnalyzer
                     string filePath = loadFile.FileName;
                     newImage = Image.FromFile(filePath);
 
-                    pictureBox1.Image = newImage;
-                    pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                    picbxImage.Image = newImage;
+                    picbxImage.SizeMode = PictureBoxSizeMode.Zoom;
 
                     szImageSize = new Size(newImage.Width, newImage.Height);
 
                     imageDimensions = newImage.Width * newImage.Height;
+
+                    lblMessageLabel.Text = filePath;
                 }
 
                 catch (SecurityException ex)
@@ -125,9 +274,9 @@ namespace ImageAnalyzer
             }
         }
 
-        private void BuildColorTree(ref TreeNode colorTree, StructColors[,] pixelRA )
+        private void BuildColorTree(ref TreeNode colorTree, StructColors[,] pixelRA)
         {
-            for(int loopCountH = 0; loopCountH < newImage.Height; loopCountH++)
+            for (int loopCountH = 0; loopCountH < newImage.Height; loopCountH++)
             {
                 for (int loopCountW = 0; loopCountW < newImage.Width; loopCountW++)
                 {
@@ -135,6 +284,7 @@ namespace ImageAnalyzer
                 }
             }
         }
+
         private void CompareColorIndex(TreeNode lclColorTree, ref int greatestCount, ref string strHexVal)
         {
             if (lclColorTree.less != null)
@@ -153,7 +303,7 @@ namespace ImageAnalyzer
                 CompareColorIndex(lclColorTree.greater, ref greatestCount, ref strHexVal);
             }
         }
-        
+
         private void IndexPixelColors(ref StructColors[,] clrLocalColorRA)
         {
             clrLocalColorRA = new StructColors[newImage.Height, newImage.Width];
@@ -173,12 +323,52 @@ namespace ImageAnalyzer
             }
         }
 
+        //private void IndexPixelColors(object data)
+        //{
+        //    int loopCountW;
+        //    int loopCountH;
+
+        //    int direction = int.Parse(data.ToString());
+
+        //    if (direction > 0)
+        //    {
+        //        loopCountH = 0;
+        //        loopCountW = 0;
+        //    }
+
+        //    else
+        //    {
+        //        loopCountH = newImage.Height-1;
+        //        loopCountW = newImage.Width-1;
+        //    }
+
+        //    Bitmap bmp = new Bitmap(newImage);
+
+        //    while (loopCountH != newImage.Height/2)
+        //    {
+        //        while (loopCountW != newImage.Width/2)
+        //        {
+        //            Color pixel = bmp.GetPixel(loopCountW, loopCountH);
+        //            pixelColorRA[loopCountH, loopCountW].R = pixel.R;
+        //            pixelColorRA[loopCountH, loopCountW].G = pixel.G;
+        //            pixelColorRA[loopCountH, loopCountW].B = pixel.B;
+
+        //            pixelColorRA[loopCountH, loopCountW].strHexValue = string.Format("{0:X2}{1:X2}{2:X2}", pixel.R, pixel.G, pixel.B);
+
+        //            loopCountW += direction;
+        //        }
+        //        loopCountH += direction;
+        //    }
+
+        //    checksum++;
+        //}
+
         public int determineImageScaleFactor()
         {
             int height = newImage.Height;
             int width = newImage.Width;
 
-            int scaleFactor = height / pictureBox1.Height;
+            int scaleFactor = height / picbxImage.Height;
 
             return scaleFactor;
         }
